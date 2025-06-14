6ccc9a7532196e9faac45541cc3a501fb5fd2c93
thebooleanguy <tharindus0601@gmail.com>
2025-06-14 18:39:09 +0530

    Use Embla instead of Swiper and create photo carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import emblaCarouselSvelte from "embla-carousel-svelte";
+	import Autoplay from "embla-carousel-autoplay";
+
+	let options = { loop: true };
+	let plugins = [Autoplay()];
+</script>
+
+<div class="embla" use:emblaCarouselSvelte={{ options, plugins }}>
+	<div class="embla__container">
+		<div class="embla__slide">
+			<img src="photos/hotel-front-1.jpg" alt="" />
+		</div>
+		<div class="embla__slide"><img src="photos/front-desk-1.jpg" alt="" /></div>
+		<div class="embla__slide"><img src="photos/food-1.jpg" alt="" /></div>
+		<div class="embla__slide"><img src="photos/room-1.jpg" alt="" /></div>
+
+		<div class="embla__slide"><img src="photos/tea-1.jpg" alt="" /></div>
+	</div>
+</div>
+
+<style>
+	.embla {
+		overflow: hidden;
+	}
+	.embla__container {
+		display: flex;
+		height: 800px;
+	}
+	.embla__slide {
+		background: #ccc;
+		flex: 0 0 100%;
+		min-width: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
